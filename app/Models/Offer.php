4cf3934c58e1ca241a186a78be0253da4e49c92d
ayoubsofi <ayoub@email.com>
2026-07-15 16:13:37 +0100

    add Eloquent relationships to Offer model

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    //
+         public function user()
+            {
+                return $this->belongsTo(User::class);
+            }
+
+        public function skills()
+        {
+            return $this->belongsToMany(
+                Skill::class,
+                'offer_skill'
+            );
+        }
+
+        public function candidatures()
+        {
+            return $this->hasMany(Candidature::class);
+        }
 }
