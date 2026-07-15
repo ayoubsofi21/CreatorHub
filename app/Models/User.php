dba448b1bf39d3c093b368be2ae413de5b02e591
ayoubsofi <ayoub@email.com>
2026-07-15 16:17:40 +0100

    add offers relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function offers()
+    {
+        return $this->hasMany(Offer::class);
+    }
 }
---
+++ database/migrations/2026_07_15_121340_create_offer_skill_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('offer_skill', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('offer_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+
+        $table->timestamps();
+
+        $table->unique(['offer_id', 'skill_id']);
+    }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offer_skill');
+    }
+};
